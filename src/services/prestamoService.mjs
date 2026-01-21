c9cf83a2561a2342cf8ad6913f2e72f44aaecf84
Alba <barrosomartinalba@gmail.com>
2026-01-21 23:16:21 +0100

    feat: service de usuario y prestamo

--- src/services/prestamoService.mjs
+++ src/services/prestamoService.mjs
@@ -1,5 +1,129 @@
 import { PrestamoRepository } from '../repositories/prestamoRepository.mjs';
+import { LibroRepository } from '../repositories/libroRepository.mjs';
+import { UsuarioRepository } from '../repositories/usuarioRepository.mjs';
 
 export class PrestamoService{
+    constructor(){
+        this.repository = new PrestamoRepository();
+        this.libroRepository = new LibroRepository();
+        this.usuarioRepository = new UsuarioRepository();
+    }
 
+    //CREAR PRESTAMO NUEVO
+    async crearPrestamo(datosPrestamo){
+        const{libro_id,usuario_id,fecha_prestamo} = datosPrestamo;
+
+        // VALIDACIONES
+        //comprobar que tiene un id de libro
+        if(!libro_id || isNaN(libro_id)){
+            throw new Error ('El ide del libro es obligatorio')
+        }
+        //comprobar que tiene un id de usuario
+        if(!usuario_id || isNaN(usuario_id)){
+            throw new Error ('El ide del usuario es obligatorio')
+        }
+        //comprobar que el libro existe
+        const libro = await this.LibroRepository.buscarPorId(libro_id);
+        if(!libro){
+            throw new Error ('El libro no existe')
+        }
+        //comprobar que el usuario existe
+        const usuario = await this.UsuarioRepository.buscarPorId(usuario_id);
+        if(!usuario){
+            throw new Error ('El usuario no existe')
+        }
+        //comprobar que hay stock
+        if (libro.stock <= 0) {
+            throw new Error('No hay stock disponible');
+        }
+
+        //LOGICA DE NEGOCIO -- REDUCIR STOCK EN 1
+        await this.libroRepository.actualizarStock(libro_id,libro.stock-1);
+
+        //CREAR
+        const prestamo = await this.repository.crear({
+            libro_id,
+            usuario_id,
+            fecha_prestamo: fecha_prestamo,
+            estado: 'ACTIVO'
+        });
+
+        return prestamo;
+    }
+
+    //BUSCAR-MOSTRAR PRESTAMO POR ID
+    async buscarPrestamo(id){
+
+        // VALIDACIONES
+        //comprobar que el id es un numero valido
+        if(!id || isNaN(id)){
+            throw new Error ('El id del prestamo no es valido')
+        }
+
+        //BUSCAR - MOSTRAR
+        const prestamo = await this.repository.buscarPorId(id);
+        if(!prestamo){
+            throw new Error ('No se ha encontrado el prestamo con el id: ',id);
+        }
+
+        return prestamo;
+    }
+
+    // BUSCAR-MOSTRAR TODOS LOS PRESTAMOS
+    async mostrarPrestamos(){
+        return await this.repository.buscarTodos();
+    }
+
+    // ACTUALIZAR ESTADO DEL PRESTAMO
+    async actualizarEstado(id, nuevoEstado){
+
+        // VALIDACIONES
+        //comprobar que el id es un numero valido
+        if(!id || isNaN(id)){
+            throw new Error ('El id del usuario no es valido')
+        }
+        //comprobar que tenga un estado
+        if(!nuevoEstado){
+            throw new Error ('El estado del prestamo es obligatorio')
+        }
+        //comprobar que el estado es ACTIVO o DEVUELTO
+        if(nuevoEstado !== 'ACTIVO' && nuevoEstado !== 'DEVUELTO'){
+            throw new Error ('El estado del prestamo no es valido, tiene que ser activo o devuelto')
+        }
+        //buscamos el prestamo
+        const prestamo = await this.repository.buscarPorId(id);
+        if(!prestamo){
+            throw new Error ('Prestamo no encontrado');
+        }
+        //LOGICA DE NEGOCIO -- AUMENTAR STOCK EN 1 SI EL ESTADO CAMBIA A DEVUELTO
+        const libro = await this.libroRepository.buscarPorId(prestamo.libro_id);
+        // pasa a devuelto +1
+        if (prestamo.estado === 'ACTIVO' && nuevoEstado === 'DEVUELTO') {
+            await this.libroRepository.actualizarStock(prestamo.libro_id, libro.stock + 1);
+        }
+
+        return await this.repository.actualizar(id, { estado: nuevoEstado });
+    }
+
+    // ELIMINAR PRESTAMO POR ID
+    async eliminarUsuario(id){
+
+        // VALIDACIONES
+        //comprobar que el id es un numero valido
+        if(!id || isNaN(id)){
+            throw new Error ('El id del prestamo no es valido')
+        }
+        //buscamos que existe el prestamo con el id indicado
+        const prestamo = await this.repository.buscarPorId(id);
+        if(!prestamo){
+            throw new Error ('No se ha encontrado el prestamo con el id: ',id);
+        }
+        //si el estado del prestamo es activo al eliminarlo se devuelve el libro +1 en stock
+        if(prestamo.estado === 'ACTIVO'){
+            const libro = await this.libroRepository.buscarPorId(prestamo.libro_id);
+            await this.libroRepository.actualizarStock(prestamo.libro_id, libro.stock + 1);
+        }
+        // ELIMINAR
+        return await this.repository.eliminar(id);
+    }
 }
--- src/services/usuarioService.mjs
+++ src/services/usuarioService.mjs
@@ -1,5 +1,118 @@
 import { UsuarioRepository } from '../repositories/usuarioRepository.mjs';
 
 export class UsuarioService{
+    constructor(){
+        this.repository = new UsuarioRepository();
+    }
 
+    // CREAR USUARIO NUEVO
+    async crearUsuario(datosUsuario){
+        const {nombre_completo,email} = datosUsuario;
+
+        // VALIDACIONES
+        //comprobar que el nombre no esta vacio
+        if(!nombre_completo || nombre_completo.trim() == '' ){
+            throw new Error ('El nombre del usuario es obligatorio.')
+        }
+        //el nombre tiene que tener minimo 3 caracteres
+        if(nombre_completo.trim().length <3){
+            throw new Error ('El nombre del usuario tiene que tener minimo 3 caracteres')
+        }
+        //comprobar que el email no esta vacio
+        if(!email || email.trim() ==''){
+            throw new Error ('El email es obligatorio.')
+        }
+        //formato de email valido
+        const emailValido = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailValido.test(email)) {
+            throw new Error('El formato de email no es valido');
+        }
+        //comprobar que el email no este en uso ya (lo ponemos pero realmente ya esta en la bbdd como unico)
+        const existeEmail = await this.repository.buscarPorEmail(email);
+        if(existeEmail){
+            throw new Error ('El email ya esta en uso')
+        }
+
+        //CREAR
+        const usuario = await this.repository.crear({
+            nombre_completo: nombre_completo.trim(),
+            email: email.trim().toLowerCase() //guardamos el email en minusculas
+        });
+
+        return usuario;
+    }
+
+    // BUSCAR-MOSTRAR UN USUARIO POR ID
+    async buscarUsuario(id){
+
+        // VALIDACIONES
+        //comprobar que el id es un numero valido
+        if(!id || isNaN(id)){
+            throw new Error ('El id del usuario no es valido')
+        }
+
+        //BUSCAR - MOSTRAR
+        const usuario = await this.repository.buscarPorId(id);
+        if(!usuario){
+            throw new Error ('No se ha encontrado el usuario con el id: ',id);
+        }
+
+        return usuario;
+    }
+
+    // BUSCAR-MOSTRAR TODOS LOS USUARIOS
+    async mostrarUsuarios(){
+        return await this.repository.buscarTodos();
+    }
+
+    // ACTUALIZAR EMAIL
+    async actualizarEmail(id, nuevoEmail){
+
+        // VALIDACIONES
+        //comprobar que el id es un numero valido
+        if(!id || isNaN(id)){
+            throw new Error ('El id del usuario no es valido')
+        }
+        //buscamos que existe el usuario con el id indicado
+        const usuario = await this.repository.buscarPorId(id);
+        if(!usuario){
+            throw new Error ('No se ha encontrado el usuario con el id: ',id);
+        }
+        //comprobar que el email no esta vacio
+        if(!nuevoEmail || nuevoEmail.trim() ==''){
+            throw new Error ('El email es obligatorio.')
+        }
+        //formato de email valido
+        const emailValido = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailValido.test(nuevoEmail)) {
+            throw new Error('El formato de email no es valido');
+        }
+        //comprobar que el email no este en uso ya (lo ponemos pero realmente ya esta en la bbdd como unico)
+        const existeEmail = await this.repository.buscarPorEmail(nuevoEmail);
+        if(existeEmail){
+            throw new Error ('El email ya esta en uso')
+        }
+
+        //ACTUALIZAR
+        return await this.repository.actualizarEmail(id, {email:nuevoEmail.trim().toLowerCase()});
+    }
+    
+    // ELIMINAR USUARIO POR ID
+    async eliminarUsuario(id){
+
+        // VALIDACIONES
+        //comprobar que el id es un numero valido
+        if(!id || isNaN(id)){
+            throw new Error ('El id del usuario no es valido')
+        }
+
+        //buscamos que existe el usuario con el id indicado
+        const usuario = await this.repository.buscarPorId(id);
+        if(!usuario){
+            throw new Error ('No se ha encontrado el usuario con el id: ',id);
+        }
+
+        // ELIMINAR
+        return await this.repository.eliminar(id);
+    }
 }
