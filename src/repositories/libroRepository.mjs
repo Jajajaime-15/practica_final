0dac6b4fe6c4a33a0006bb6fc5922f4ab4ef479a
Alba <barrosomartinalba@gmail.com>
2026-01-21 13:35:02 +0100

    feat: repositorios libro y usuario

--- src/repositories/libroRepository.mjs
+++ src/repositories/libroRepository.mjs
@@ -3,4 +3,52 @@ import { Libro } from '../models/Libro.mjs';
 
 export class LibroRepository{
 
+    // CREAR LIBRO EN BBDD
+    async crear (data){
+        const {data, error} = await supabase //peticion 'insert' a supabase
+            .from ('libros')
+            .insert([data])
+            .select()
+            .single();
+
+        if (error) throw error;
+        return new Libro(data); //convertimos los datos en un objeto Libro y lo devolvemos
+    }
+
+    // BUSCAR-MOSTRAR UN LIBRO POR ID
+    async buscarPorId(id) {
+        const { data, error } = await supabase //peticion 'select' a supabase con condicion (solo queremos un libro)
+            .from('libros')
+            .select('*', autores (nombre_completo)) //join con la tabla de autores para coger el nombre del autor del libro
+            .eq('id', id) //condicion: id indicado coincide con un id de la tabla
+            .single();
+
+        if (error) return null;
+        return data ? new Libro(data) : null; //los datos encontrados los convertimos en objeto Libro, si no hay datos = 'null'
+    }
+
+    // BUSCAR-MOSTRAR TODOS LOS LIBROS
+    async buscarTodos() {
+        const { data, error } = await supabase //peticion 'select' a supabase sin condicion (queremos todos los libros)
+            .from('libros')
+            .select('*', autores (nombre_completo))
+            .order('titulo', { ascending: true }); //los libros los mostramos ordenados alfabeticamente
+
+        if (error) throw error;
+        return data.map(item => new Libro(item)); //convertimos cada registro del array en objeto Libro
+    }
+
+    // ACTUALIZAR STOCK DE UN LIBRO
+
+
+    // ELIMINAR UN LIBRO POR ID DE LA BBDD
+    async eliminar(id) {
+        const { error } = await supabase //peticion 'delete' a supabase
+            .from('libros')
+            .delete()
+            .eq('id', id); //condicion: id indicado coincide con un id de la tabla
+
+        if (error) throw error;
+        return true;
+    }
 }
--- src/repositories/usuarioRepository.mjs
+++ src/repositories/usuarioRepository.mjs
@@ -3,4 +3,62 @@ import { Usuario } from '../models/Usuario.mjs';
 
 export class UsuarioRepository{
 
+    // CREAR USUARIO EN BBDD
+    async crear (data){
+        const {data, error} = await supabase //peticion 'insert' a supabase
+            .from ('usuarios')
+            .insert([data])
+            .select()
+            .single();
+
+        if (error) throw error;
+        return new Usuario(data); //convertimos los datos en un objeto Usuario y lo devolvemos
+    }
+
+    // BUSCAR-MOSTRAR UN USUARIO POR ID
+    async buscarPorId(id) {
+        const { data, error } = await supabase //peticion 'select' a supabase con condicion (solo queremos un usuario)
+            .from('ausuarios')
+            .select('*')
+            .eq('id', id) //condicion: id indicado coincide con un id de la tabla
+            .single();
+
+        if (error) return null;
+        return data ? new Libro(data) : null; //los datos encontrados los convertimos en objeto Usuario, si no hay datos = 'null'
+    }
+
+    // BUSCAR-MOSTRAR TODOS LOS USUARIOS
+    async buscarTodos() {
+        const { data, error } = await supabase //peticion 'select' a supabase sin condicion (queremos todos los libros)
+            .from('usuarios')
+            .select('*')
+            .order('nombre_completo', { ascending: true }); //los libros los mostramos ordenados alfabeticamente
+
+        if (error) throw error;
+        return data.map(item => new Libro(item)); //convertimos cada registro del array en objeto Libro
+    }
+
+    // ACTUALIZAR EMAIL DE UN USUARIO
+    async actualizarEmail(id,cambios){
+        const {data,error} = await supabase //peticion 'update' a supabase
+            .from('usuarios')
+            .update('cambios') //actualizamos los datos
+            .eq('id',id)
+            .single();
+        
+        if (error) throw error;
+        return new Usuario(data)
+    }
+
+
+    // ELIMINAR UN USUARIO POR ID DE LA BBDD
+    async eliminar(id) {
+        const { error } = await supabase //peticion 'delete' a supabase
+            .from('usuarios')
+            .delete()
+            .eq('id', id); //condicion: id indicado coincide con un id de la tabla
+
+        if (error) throw error;
+        return true;
+    }
 }
