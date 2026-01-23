8eb079d8018368aae5e9a96ed9d65e7fbd478aae
Jaime Ruiz <jajajaime1503@gmail.com>
2026-01-23 01:09:39 +0100

    fix: cache redis arreglado en consultas frecuentes, añadidas invalidaciones de cache

--- src/repositories/autorRepository.mjs
+++ src/repositories/autorRepository.mjs
@@ -3,69 +3,10 @@ import { Autor } from '../models/Autor.mjs';
 import redis from '../config/redis.mjs';
 
 export class AutorRepository {
-
-  constructor() {
-    this.r = redis;
-    this.TTL = 60; // Tiempo de caché en segundos
-  }
-  async obtenerAutores() {
-    const key = 'autores:lista';
-
-    let autoresCache = null;
-    try {
-      if (this.r) {
-        autoresCache = await this.r.get(key);
-        if (autoresCache) {
-          await this.r.expire(key, this.TTL); // Renovar TTL
-        }
-      }
-    } catch (error) {
-      console.log('Error al acceder a la cache:', error.message);
-    }
-
-    if (autoresCache) {
-      console.log('Autores encontrados en la cache');
-      return JSON.parse(autoresCache).map(item => new Autor(item));
-    }
-
-    // Si no está en cache asi que buscamos en la base de datos
-    console.log('Autores no encnontrados en cache, consultando base de datos');
-
-    try {
-      const { data, error } = await supabase
-        .from('autores')
-        .select('*')
-        .order('nombre_completo', { ascending: true });
-
-      if (error) {
-        console.error('Error consultando en la base de datos:', error.message);
-        throw new Error(`Error al obtener autores: ${error.message}`);
-      }
-
-      if (!data || data.length === 0) {
-        console.log(' No hay autores en la base de datos');
-        return [];
-      }
-
-      console.log(`${data.length} autores obtenidos de la base de datos`);
-
-      // Guardamos los datos en cache para proximas consultas
-      try {
-        if (this.r) {
-          await this.r.set(key, JSON.stringify(data) , { EX: this.TTL });
-          console.log(' Autores guardados en caché');
-        }
-      } catch (error) {
-        console.log('Error guardando en cache:', error.message);
-      }
-
-      // Convertir a objetos Libro
-      return data.map(item => new Autor(item));
-
-    } catch (error) {
-      console.error('Error al obtener autores', error);
-      throw error;
-    }
+  constructor(){
+    this.redis = redis
+    this.TTL = 60
+    this.CACHE_KEY = 'autores:lista' // convencion de nombres de redis, significa que almacena el listado de autores
   }
 
   // CREAR AUTOR EN BBDD
@@ -77,6 +18,14 @@ export class AutorRepository {
       .single();
 
     if (error) throw error;
+    try{ // en caso de que se cree una nueva instancia en la bbdd ya no nos servira lo guardado en cache, por lo que realizamos la invalidacion de cache
+      if (this.redis){
+        await this.redis.del(this.CACHE_KEY)
+      }
+    }catch(error){
+      console.log("Error al realizar la invalidacion de cache: ", error)
+    }
+
     return new Autor(data); //convertimos los datos en un objeto Autor y lo devolvemos
   }
 
@@ -94,12 +43,36 @@ export class AutorRepository {
 
   // BUSCAR-MOSTRAR TODOS LOS AUTORES
   async buscarTodos() {
+    try{ // primero comprobamos si se puede leer del cache, ya que esta es una consulta frecuente que permitimos almacenar
+      if (this.redis){
+        const cached = await this.redis.get(this.CACHE_KEY)
+        if (cached){
+          console.log('Lista de autores obtenida del cache')
+          await this.redis.expire(this.CACHE_KEY, this.TTL) // hacemos que se renueve el ttl cada vez que se acceda, para mantener la frecuencia
+          return JSON.parse(cached).map(item => new Autor(item)) // convertimos los strings de redis en objetos y luego en instancias de la clase
+        }
+      }
+    }catch(error){
+      console.log("Error al leer cache: ", error)
+    }
+
+    console.log('Los autores han sido obtenidos de la base de datos') // en caso de no estar en el cache procedemos como siempre, accediendo a la base de datos
     const { data, error } = await supabase //peticion 'select' a supabase sin condicion (queremos todos los autores)
       .from('autores')
       .select('*')
       .order('nombre_completo', { ascending: true }); //los autores los mostramos ordenados alfabeticamente
 
     if (error) throw error;
+
+    try{ // si se ha consultado de la base de datos, guardamos en cache para poder acceder de esta manera en las proximas consultas
+      if (this.redis && data){
+        await this.redis.setEx(this.CACHE_KEY, this.TTL, JSON.stringify(data)) // asignamos la clave donde guardamos los datos, el tiempo que duraran y los datos obtenidos de la bbdd
+        console.log('La lista de autores ha sido guardada en cache')
+      }
+    }catch(error){
+      console.log('Error al guardar en cache: ', error)
+    }
+
     return data.map(item => new Autor(item)); //convertimos cada registro del array en objeto Autor
   }
 
@@ -111,7 +84,15 @@ export class AutorRepository {
       .eq('id', id); //condicion: id indicado coincide con un id de la tabla
 
     if (error) throw error;
+
+    try{ // en caso de que se elimine una instancia en la bbdd ya no nos servira lo guardado en cache, por lo que realizamos la invalidacion de cache
+      if (this.redis){
+        await this.redis.del(this.CACHE_KEY)
+      }
+    }catch(error){
+      console.log("Error al realizar la invalidacion de cache: ", error)
+    }
+
     return true;
   }
-
 }
--- src/repositories/libroRepository.mjs
+++ src/repositories/libroRepository.mjs
@@ -3,128 +3,117 @@ import { Libro } from '../models/Libro.mjs';
 import redis from '../config/redis.mjs';
 
 export class LibroRepository{
-    constructor() {
-    
-    this.r = redis;
-    this.TTL = 60; // Tiempo de caché en segundos
+  constructor(){
+    this.redis = redis
+    this.TTL = 60
+    this.CACHE_KEY = 'libros:lista' // convencion de nombres de redis, significa que almacena el listado de libros
   }
-    async obtenerLibros() {
-    const key = 'libros:lista';
-    
-    let librosCache = null;
-    try {
-      if (this.r) {
-        librosCache = await this.r.get(key);
-        if (librosCache) {
-          await this.r.expire(key, this.TTL); // Renovar TTL
-        }
+  // CREAR LIBRO EN BBDD
+  async crear (libroData){
+    const {data, error} = await supabase //peticion 'insert' a supabase
+      .from ('libros')
+      .insert([libroData])
+      .select()
+      .single();
+
+    if (error) throw error;
+
+    try{ // en caso de que se cree una nueva instancia en la bbdd ya no nos servira lo guardado en cache, por lo que realizamos la invalidacion de cache
+      if (this.redis){
+        await this.redis.del(this.CACHE_KEY)
       }
-    } catch (error) {
-      console.log('Error al acceder a la cache:', error.message);
+    }catch(error){
+      console.log("Error al realizar la invalidacion de cache: ", error)
     }
-    
-    if (librosCache) {
-      console.log('Libros encontrados en la cache');
-      return JSON.parse(librosCache).map(item => new Libro(item));
-    }
-    
-    // Si no está en cache asi que buscamos en la base de datos
-    console.log('Libros no encontrados en cache, consultando base de datos');
-    
-    try {
-      const { data, error } = await supabase
-        .from('libros')
-        .select('*')
-        .order('titulo', { ascending: true });
-      
-      if (error) {
-        console.error('Error consultando en la base de datos:', error.message);
-        throw new Error(`Error al obtener libros: ${error.message}`);
-      }
-      
-      if (!data || data.length === 0) {
-        console.log(' No hay libros en la base de datos');
-        return [];
-      }
-      
-      console.log(`${data.length} libros obtenidos de la base de datos`);
-      
-      // Guardamos los datos en cache para proximas consultas
-      try {
-        if (this.r) {
-          await this.r.set(key, JSON.stringify(data), { EX: this.TTL });
-          console.log(' Libros guardados en caché');
-        }
-      } catch (error) {
-        console.log('Error guardando en cache:', error.message);
-      }
-      
-      // Convertir a objetos Libro
-      return data.map(item => new Libro(item));
-      
-    } catch (error) {
-      console.error('Error al obtener libros', error);
-      throw error;
-    }
+
+    return new Libro(data); //convertimos los datos en un objeto Libro y lo devolvemos
   }
 
+  // BUSCAR-MOSTRAR UN LIBRO POR ID
+  async buscarPorId(id) {
+    const { data, error } = await supabase //peticion 'select' a supabase con condicion (solo queremos un libro)
+      .from('libros')
+      .select('*') 
+      .eq('id', id) //condicion: id indicado coincide con un id de la tabla
+      .single();
 
-    // CREAR LIBRO EN BBDD
-    async crear (libroData){
-        const {data, error} = await supabase //peticion 'insert' a supabase
-            .from ('libros')
-            .insert([libroData])
-            .select()
-            .single();
+    if (error) return null;
+    return data ? new Libro(data) : null; // los datos encontrados los convertimos en objeto Libro, si no hay datos = 'null'
+  }
 
-        if (error) throw error;
-        return new Libro(data); //convertimos los datos en un objeto Libro y lo devolvemos
+  // BUSCAR-MOSTRAR TODOS LOS LIBROS
+  async buscarTodos() {
+    try{ // primero comprobamos si se puede leer del cache, ya que esta es una consulta frecuente que permitimos almacenar
+      if (this.redis){
+        const cached = await this.redis.get(this.CACHE_KEY)
+        if (cached){
+          console.log('Lista de libros obtenida del cache')
+          await this.redis.expire(this.CACHE_KEY, this.TTL) // hacemos que se renueve el ttl cada vez que se acceda, para mantener la frecuencia
+          return JSON.parse(cached).map(item => new Libro(item)) // convertimos los strings de redis en objetos y luego en instancias de la clase
+        }
+      }
+    }catch(error){
+      console.log("Error al leer cache: ", error)
+    }
+    console.log('Los libros han sido obtenidos de la base de datos') // en caso de no estar en el cache procedemos como siempre, accediendo a la base de datos
+    const { data, error } = await supabase //peticion 'select' a supabase sin condicion (queremos todos los libros)
+      .from('libros')
+      .select('*')
+      .order('titulo', { ascending: true }); //los libros los mostramos ordenados alfabeticamente
+
+    if (error) throw error;
+
+    try{ // si se ha consultado de la base de datos, guardamos en cache para poder acceder de esta manera en las proximas consultas
+      if (this.redis && data){
+        await this.redis.setEx(this.CACHE_KEY, this.TTL, JSON.stringify(data)) // asignamos la clave donde guardamos los datos, el tiempo que duraran y los datos obtenidos de la bbdd
+        console.log('La lista de libros ha sido guardada en cache')
+      }
+    }catch(error){
+      console.log('Error al guardar en cache: ', error)
     }
 
-    // BUSCAR-MOSTRAR UN LIBRO POR ID
-    async buscarPorId(id) {
-        const { data, error } = await supabase //peticion 'select' a supabase con condicion (solo queremos un libro)
-            .from('libros')
-            .select('*') 
-            .eq('id', id) //condicion: id indicado coincide con un id de la tabla
-            .single();
+    return data.map(item => new Libro(item)); //convertimos cada registro del array en objeto Libro
+  }
 
-        if (error) return null;
-        return data ? new Libro(data) : null; // los datos encontrados los convertimos en objeto Libro, si no hay datos = 'null'
+  // ACTUALIZAR STOCK DE UN LIBRO
+  async actualizarStock(id, nuevoStock){
+    const {data,error} = await supabase // peticion 'update' a supabase
+      .from('libros')
+      .update({stock: nuevoStock}) // indicamos el campo que actualizamos
+      .eq('id',id)
+      .select()
+      .single();
+    
+    if (error) throw error;
+
+    try{ // en caso de que se actualice una instancia en la bbdd ya no nos servira lo guardado en cache, por lo que realizamos la invalidacion de cache
+      if (this.redis){
+        await this.redis.del(this.CACHE_KEY)
+      }
+    }catch(error){
+      console.log("Error al realizar la invalidacion de cache: ", error)
     }
 
-    // BUSCAR-MOSTRAR TODOS LOS LIBROS
-    async buscarTodos() {
-        const { data, error } = await supabase //peticion 'select' a supabase sin condicion (queremos todos los libros)
-            .from('libros')
-            .select('*')
-            .order('titulo', { ascending: true }); //los libros los mostramos ordenados alfabeticamente
+    return new Libro(data);
+  }
 
-        if (error) throw error;
-        return data.map(item => new Libro(item)); //convertimos cada registro del array en objeto Libro
+  // ELIMINAR UN LIBRO POR ID DE LA BBDD
+  async eliminar(id) {
+    const { error } = await supabase //peticion 'delete' a supabase
+      .from('libros')
+      .delete()
+      .eq('id', id); //condicion: id indicado coincide con un id de la tabla
+
+    if (error) throw error;
+
+    try{ // en caso de que se elimine una instancia en la bbdd ya no nos servira lo guardado en cache, por lo que realizamos la invalidacion de cache
+      if (this.redis){
+        await this.redis.del(this.CACHE_KEY)
+      }
+    }catch(error){
+      console.log("Error al realizar la invalidacion de cache: ", error)
     }
 
-    // ACTUALIZAR STOCK DE UN LIBRO
-    async actualizarStock(id, nuevoStock){
-        const {data,error} = await supabase // peticion 'update' a supabase
-            .from('libros')
-            .update({stock: nuevoStock}) // indicamos el campo que actualizamos
-            .eq('id',id)
-            .select()
-            .single();
-        
-        if (error) throw error;
-        return new Libro(data);
-    }
-
-    // ELIMINAR UN LIBRO POR ID DE LA BBDD
-    async eliminar(id) {
-        const { error } = await supabase //peticion 'delete' a supabase
-            .from('libros')
-            .delete()
-            .eq('id', id); //condicion: id indicado coincide con un id de la tabla
-
-        if (error) throw error;
-        return true;
-    }
+    return true;
+  }
 }
